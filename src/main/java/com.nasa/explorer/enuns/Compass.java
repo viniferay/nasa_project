6fb72cd1f1e616aa891973a0545bf4abe5fd3319
Vinicius <vinicius.feray@gmail.com>
2022-03-02 13:52:40 -0300

    Create method getDirection in Compass enum

--- src/main/java/com.nasa/explorer/enuns/Compass.java
+++ src/main/java/com.nasa/explorer/enuns/Compass.java
@@ -1,5 +1,8 @@
 package com.nasa.explorer.enuns;
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum Compass {
     NORTH(0, 1),
     EAST(1, 0),
@@ -25,4 +28,15 @@ public enum Compass {
         return axleY;
     }
 
+    public Compass getDirection(Command command){
+        List<Compass> compass = Arrays.asList(Compass.values());
+        int index = compass.indexOf(this);
+
+        if(index <= 0  && command == Command.LEFT){
+            return compass.get(compass.size()-1);
+        }else if(index >= compass.size()-1 && command == Command.RIGTH){
+            return compass.get(0);
+        }
+        return Compass.values()[index+command.getValue()];
+    }
 }
