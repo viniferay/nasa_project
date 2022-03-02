a4b12134edcf5b021a9948c4c66ac6e77e8c60db
Vinicius <vinicius.feray@gmail.com>
2022-03-02 12:43:54 -0300

    Create Explorer Class

---
+++ src/main/java/com.nasa/explorer/Explorer.java
@@ -0,0 +1,34 @@
+package com.nasa.explorer;
+
+import com.nasa.explorer.enuns.Compass;
+
+public class Explorer {
+    private Compass direction;
+    private int positionY;
+    private int positionX;
+
+    public Compass getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Compass direction) {
+        this.direction = direction;
+    }
+
+    public int getPositionY() {
+        return positionY;
+    }
+
+    public void setPositionY(int positionY) {
+        this.positionY = positionY;
+    }
+
+    public int getPositionX() {
+        return positionX;
+    }
+
+    public void setPositionX(int positionX) {
+        this.positionX = positionX;
+    }
+    
+}
