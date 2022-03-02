3c714f4a11e152a065415bd6fa9a09234158ca6e
Vinicius <vinicius.feray@gmail.com>
2022-03-02 14:01:48 -0300

    Inital input and output sistem

---
+++ src/main/java/com.nasa/io/Input.java
@@ -0,0 +1,11 @@
+package com.nasa.io;
+
+import java.util.Scanner;
+
+public class Input {
+
+    private static Scanner scanner(){
+        return new Scanner(System.in);
+    }
+
+}
---
+++ src/main/java/com.nasa/io/Output.java
@@ -0,0 +1,8 @@
+package com.nasa.io;
+
+public class Output {
+
+    public static void print(String mensagem){
+        System.out.println(mensagem);
+    }
+}
