722c885417903d80ed5da7d74844ffaa2dbe4047
Vinicius <vinicius.feray@gmail.com>
2022-03-07 19:52:45 -0300

    Create move explorer method

--- src/main/java/com.nasa/explorer/ExplorerService.java
+++ src/main/java/com.nasa/explorer/ExplorerService.java
@@ -1,8 +1,18 @@
 package com.nasa.explorer;
 
+import com.nasa.explorer.dtos.ExplorerDTO;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ExplorerService {
-    
+
+       public Explorer moveExplorer(ExplorerDTO explorerDTO){
+           Explorer explorer = new Explorer(explorerDTO.getDirection(), explorerDTO.getPositionY(),
+                   explorerDTO.getPositionX());
+
+           for (String command : explorerDTO.getInstructions().split("")){
+               explorer.playCommand(command);
+           }
+            return explorer;
+       }
 }
