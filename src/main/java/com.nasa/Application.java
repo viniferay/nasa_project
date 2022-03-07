8357ab984ad7da0b1c828f3cc6dd890204da9f55
Vinicius <vinicius.feray@gmail.com>
2022-03-07 16:32:15 -0300

    Implement Spring core

--- src/main/java/com.nasa/Application.java
+++ src/main/java/com.nasa/Application.java
@@ -1,7 +1,12 @@
 package com.nasa;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
     
     public static void main(String[] args) {
+        SpringApplication.run(Application.class);
     }
 }
