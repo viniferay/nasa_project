042b2bfb78a45721fdaa49c48dd774b2c86109c5
Vinicius <vinicius.feray@gmail.com>
2022-03-07 21:41:34 -0300

    Implement Swagger

--- src/main/java/com.nasa/explorer/ExplorerController.java
+++ src/main/java/com.nasa/explorer/ExplorerController.java
@@ -1,6 +1,8 @@
 package com.nasa.explorer;
 
 import com.nasa.explorer.dtos.ExplorerDTO;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +16,7 @@ public class ExplorerController {
     private ExplorerService explorerService;
 
     @PutMapping
+    @ApiResponses(@ApiResponse(code = 422, message = "Informações que não podem ser processadas por falta de compatibilidade"))
     public Explorer moveExplorer(@RequestBody @Valid ExplorerDTO explorerDTO){
         return explorerService.moveExplorer(explorerDTO);
     }
